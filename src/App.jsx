fc58e05552c365a632e6128d842b127e501ff5be
KD191996 <katyushadanilenko82@gmail.com>
2024-07-06 20:31:18 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+import Card from './components/Card.jsx'
+import React from 'react';
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <Card />
+                <Card title="hello 1" />
+                <Card text="how are you? 2" />
+                <Card title="hello" text="how are you? 3" />
             </div>
         );
     }
