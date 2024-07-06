4e17ffb6b73c05e60d5c7bd06dc66214e821399d
KD191996 <katyushadanilenko82@gmail.com>
2024-07-06 21:44:14 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
 import React from 'react';
+import Definitions from './components/Definitions.jsx';
 
 class App extends React.Component {
     render() {
+
+        const definitions = [
+            { dt: 'one', dd: 'two', id: 1 },
+            { dt: 'another term', dd: 'another description', id: 2 },
+        ];
+
+
         return (
             <div className="App">
-                <Card />
+                <Definitions data={definitions}/>
             </div>
         );
     }
