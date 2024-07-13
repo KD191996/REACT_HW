36f71fb24e0aabad56045d09be427ded72cc7d17
KD191996 <katyushadanilenko82@gmail.com>
2024-07-13 19:48:16 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
 import React from 'react';
+import Alert from './components/Alert.jsx';
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <Card />
+                <Alert type="warning" text="what is love?" />
+                <Alert type="success" text="Operation successful!" />
+                <Alert type="danger" text=" Error " />
             </div>
         );
     }
---
+++ src/components/Alert.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Alert extends React.Component {
+    render() {
+        const { type, text } = this.props;
+        const alertClass = `alert alert-${type}`;
+
+        return (
+            <div className={alertClass} role="alert">{text}</div>
+        );
+    }
+}
+export default Alert
