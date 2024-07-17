5c4bf615f7e0e2d25e93a9c002ddbce257cc2602
KD191996 <katyushadanilenko82@gmail.com>
2024-07-17 16:52:33 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Carousel from './components/Carousel.jsx';
-
+import firstImage from './images/first.jpeg';
+import secondImage from './images/second.jpeg';
+import thirdImage from './images/third.jpeg';
 
 class App extends React.Component {
 
     render() {
 
-        const images = [
-            '/images/first.jpeg',
-            '/images/second.jpeg',
-            '/images/third.jpeg'
-        ];
+        const images = [firstImage, secondImage, thirdImage];
+
+
+
         return (
             <div className="App">
                 <Carousel images={images} />
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import PropTypes from 'prop-types';
+
 class Carousel extends React.Component {
     constructor(props) {
         super(props);
@@ -26,38 +26,41 @@ class Carousel extends React.Component {
         const { currentIndex } = this.state;
 
         return (
-            <div id="carousel" className="carousel slide" data-bs-ride="carousel">
-                  <div className="carousel-inner">
-                        {images.map((img, i) => (
+            <div className="container">
+                <div id="carousel" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        {images.map((image, index) => (
                             <div
-                                key={i}
-                                className={classNames('carousel-item', { active: i === index })}
+                                key={index}
+                                className={classNames('carousel-item', { 'active': index === currentIndex })}
                             >
-                                <img alt="" className="d-block w-100" src={img} />
+                                <img src={image} className="d-block w-100" alt="" />
                             </div>
                         ))}
                     </div>
-                <button
-                    className="carousel-control-prev"
-                    data-bs-target="#carousel"
-                    type="button"
-                    data-bs-slide="prev"
-                    onClick={this.handlePrev}
-                >
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button
-                    className="carousel-control-next"
-                    data-bs-target="#carousel"
-                    type="button"
-                    data-bs-slide="next"
-                    onClick={this.handleNext}
-                >
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+                    <button
+                        className="carousel-control-prev"
+                        data-bs-target="#carousel"
+                        type="button"
+                        data-bs-slide="prev"
+                        onClick={this.handlePrev}
+                    >
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button
+                        className="carousel-control-next"
+                        data-bs-target="#carousel"
+                        type="button"
+                        data-bs-slide="next"
+                        onClick={this.handleNext}
+                    >
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
+
         );
     }
 }
