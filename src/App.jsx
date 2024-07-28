b51d2852ea227a552f2907a3a9b2b41d341aa75a
KD191996 <katyushadanilenko82@gmail.com>
2024-07-28 14:44:52 +0300

    finish commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import PostCatalog from './components/PostCatalog';
 import React from 'react';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+        };
+    }
+
+    async fetchPosts() {
+        const request = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await request.json();
+        this.setState({ posts: data });
+    }
+
+    componentDidMount() {
+        this.fetchPosts();
+    }
+
     render() {
         return (
             <div className="App">
-                <Card />
+                <h1 className='text-center mb-5 mt-5'>Post Catalog</h1>
+                <Container>
+                    <Row>
+                        <Col xs={12}>
+                            <PostCatalog
+                                data={this.state.posts}
+                                itemTitle="title"
+                                itemBody="body"
+                            />
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
 }
 
+export default App;
 
-export default App
---
+++ src/components/PostCatalog.jsx
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PostCatalog extends Component {
+    render() {
+        const { data, itemTitle, itemBody } = this.props;
+
+        return (
+            <div className="posts">
+                <ul className="postslist">
+                    {data.map((item, index) => (
+                        <li
+                            className="posts_single-post "
+                            data-post-id={item.id}
+                            key={item.id + '_' + index}
+                        >
+                            <h3 className="postspost-title">{item[itemTitle]}</h3>
+                            <p className="postspost-description">{item[itemBody]}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+PostCatalog.propTypes = {
+    data: PropTypes.array.isRequired,
+    itemTitle: PropTypes.string.isRequired,
+    itemBody: PropTypes.string.isRequired,
+};
+
+export default PostCatalog;
+
