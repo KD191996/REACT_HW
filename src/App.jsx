5f00599043b6ea9029a7d6d58ed9e19785daeb31
KD191996 <katyushadanilenko82@gmail.com>
2024-07-31 15:41:06 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import MarkdownEditor from './component/MarkdownEditor.jsx';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const handleChange = (content) => {
+    console.log(content);
+};
 
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
-                <Card />
-            </div>
+            <Container>
+                <Col xs={12}>
+                    <Row>
+                        <div className="App">
+                            <h1 className="text-center mb-4">Markdown Editor</h1>
+                            <MarkdownEditor contentChange={handleChange} />
+                        </div>
+                    </Row>
+                </Col>
+            </Container>
         );
     }
 }
