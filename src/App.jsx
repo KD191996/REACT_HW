e7026563d7a67395c4cfe5bd5be62c93d19c02a2
KD191996 <katyushadanilenko82@gmail.com>
2024-08-01 15:19:29 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import PostCatalog from './components/PostCatalog';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <Card />
-            </div>
-        );
-    }
-}
+const App = () => {
+    const [posts, setPosts] = useState([]);
 
+    const fetchPosts = async () => {
+        const request = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await request.json();
+        setPosts(data);
+    };
 
-export default App
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1 className='text-center mb-5 mt-5'>Post Catalog</h1>
+            <Container>
+                <Row>
+                    <Col xs={12}>
+                        <PostCatalog data={posts} itemTitle="title" itemBody="body" />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default App;
