df537a6ded05684a5c2fd9423c05db419b0b24f6
KD191996 <katyushadanilenko82@gmail.com>
2024-09-04 12:55:26 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import routerObjects from './routerConfig/routerObjects';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <Card />
-            </div>
-        );
-    }
-}
+const App = () => (
+    <Router>
+        <Routes>
+            {routerObjects.map((route) => (
+                <Route key={route.id} path={route.path} element={route.element} />
+            ))}
+        </Routes>
+    </Router>
+);
 
-
-export default App
+export default App;
