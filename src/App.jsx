ba3fdbb940c72c78c7a885ceefa67ec6daf949dc
KD191996 <katyushadanilenko82@gmail.com>
2024-09-14 11:14:54 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import HomePage from './pages/HomePage';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <Card />
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    );
 }
 
-
-export default App
+export default App;
